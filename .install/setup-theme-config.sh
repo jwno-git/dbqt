921691e2f6a1c0b3b60715ea2f4a50b8696448dd
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-07-27 01:25:35 -0600

    Update setup-theme-config.sh

--- .install/setup-theme-config.sh
+++ .install/setup-theme-config.sh
@@ -17,8 +17,10 @@ cp -r $HOME/.icons/BreezeX-RosePine-Linux /usr/share/icons/
 cp -r $HOME/.themes/Tokyonight-Dark /usr/share/themes/
 
 # Setup root configuration
+mkdir -p /root/.src
 mv $HOME/dbqt/.root/.config /root/
 mv $HOME/dbqt/.root/.vimrc /root/
+mv $HOME/dbqt/.root/debianroot.png /root/
 mv $HOME/dbqt/.root/tlp.conf /etc/
 
 # Move user configuration
@@ -28,9 +30,16 @@ mv $HOME/dbqt/Documents $HOME/
 mv $HOME/dbqt/Pictures $HOME/
 mv $HOME/dbqt/.vimrc $HOME/
 mv $HOME/dbqt/.bashrc $HOME/
+mv $HOME/dbqt/bookmarks.html $HOME/
 cp $HOME/.bashrc /root/
+
+# Make scripts executable
+chmod +x $HOME/.local/scripts/*.sh
 
 # Configure cursor theme
 sed -i 's/Adwaita/BreezeX-RosePine-Linux/g' /usr/share/icons/default/index.theme
+
+# Cleanup
+rm -rf $HOME/dbqt/.root
 
 echo "Themes and configuration setup complete"
