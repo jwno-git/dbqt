702895eb80ec40edebad8677ec51df217152b09c
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-07-26 18:30:48 -0600

    Create setup-theme-config.sh

---
+++ .install/setup-theme-config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+# Extract and setup themes
+cd $HOME/dbqt/.icons/
+tar -xf BreezeX-RosePine-Linux.tar.xz
+
+cd $HOME/dbqt/.themes/
+tar -xf Tokyonight-Dark.tar.xz
+
+# Move themes to user directories
+mv $HOME/dbqt/.icons $HOME/
+mv $HOME/dbqt/.themes $HOME/
+
+# Install themes system-wide
+cp -r $HOME/.icons/BreezeX-RosePine-Linux /usr/share/icons/
+cp -r $HOME/.themes/Tokyonight-Dark /usr/share/themes/
+
+# Setup root configuration
+mv $HOME/dbqt/.root/.config /root/
+mv $HOME/dbqt/.root/.vimrc /root/
+mv $HOME/dbqt/.root/tlp.conf /etc/
+
+# Move user configuration
+mv $HOME/dbqt/.config $HOME/
+mv $HOME/dbqt/.local $HOME/
+mv $HOME/dbqt/Documents $HOME/
+mv $HOME/dbqt/Pictures $HOME/
+mv $HOME/dbqt/.vimrc $HOME/
+mv $HOME/dbqt/.bashrc $HOME/
+cp $HOME/.bashrc /root/
+
+# Configure cursor theme
+sed -i 's/Adwaita/BreezeX-RosePine-Linux/g' /usr/share/icons/default/index.theme
+
+echo "Themes and configuration setup complete"
