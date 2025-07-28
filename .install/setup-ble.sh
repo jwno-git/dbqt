07b9f5d3339520fc6123f205f670647059f80059
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-07-28 13:19:54 -0600

    Create setup-ble.sh

---
+++ .install/setup-ble.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Change to source directory
+cd $HOME/src
+
+# Clone BLE.sh repository
+echo "Cloning BLE.sh repository..."
+git clone https://github.com/akinomyoga/ble.sh.git
+cd ble.sh
+
+# Build BLE.sh
+echo "Building BLE.sh..."
+make
+
+# Install system-wide to /usr/local
+echo "Installing BLE.sh to /usr/local/share/blesh..."
+make install PREFIX=/usr/local
+
+echo "BLE.sh installed successfully"
+echo "BLE.sh is now available system-wide at /usr/local/share/blesh/ble.sh"
