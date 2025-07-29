a564f3f6354b6c30f866240f51bfb8d46c130416
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-07-29 02:09:02 -0600

    Update setup-theme-config.sh

--- .install/setup-theme-config.sh
+++ .install/setup-theme-config.sh
@@ -2,49 +2,49 @@
 set -e
 
 # Extract and setup themes
-cd $HOME/dbqt/.icons/
+cd /home/jwno/dbqt/.icons/
 tar -xf BreezeX-RosePine-Linux.tar.xz
 
-cd $HOME/dbqt/.themes/
+cd /home/jwno/dbqt/.themes/
 tar -xf Tokyonight-Dark.tar.xz
 
 # Move themes to user directories
-mv $HOME/dbqt/.icons $HOME/
-mv $HOME/dbqt/.themes $HOME/
+mv /home/jwno/dbqt/.icons $HOME/
+mv /home/jwno/dbqt/.themes $HOME/
 
 # Install themes system-wide
-cp -r $HOME/.icons/BreezeX-RosePine-Linux /usr/share/icons/
-cp -r $HOME/.themes/Tokyonight-Dark /usr/share/themes/
+cp -r /home/jwno/.icons/BreezeX-RosePine-Linux /usr/share/icons/
+cp -r /home/jwno/.themes/Tokyonight-Dark /usr/share/themes/
 
 # Setup root configuration
-mv $HOME/dbqt/.root/.config /root/
-mv $HOME/dbqt/.root/tlp.conf /etc/
+mv /home/jwno/dbqt/.root/.config /root/
+mv /home/jwno/dbqt/.root/tlp.conf /etc/
 
 # Move battery toggle script
-mv $HOME/dbqt/battery-toggle /usr/local/bin/
+mv /home/jwno/dbqt/battery-toggle /usr/local/bin/
 chmod +x /usr/local/bin/battery-toggle
 
 # Move user configuration
-mv $HOME/dbqt/.config $HOME/
-mv $HOME/dbqt/.local $HOME/
+mv /home/jwno/dbqt/.config $HOME/
+mv /home/jwno/dbqt/.local $HOME/
 # mv $HOME/dbqt/Documents $HOME/
-mv $HOME/dbqt/Pictures $HOME/
-mv $HOME/dbqt/.vimrc $HOME/
-mv $HOME/dbqt/.bashrc $HOME/
-mv $HOME/dbqt/.xinitrc $HOME/
-mv $HOME/dbqt/.Xresources $HOME/
+mv /home/jwno/dbqt/Pictures $HOME/
+mv /home/jwno/dbqt/.vimrc $HOME/
+mv /home/jwno/dbqt/.bashrc $HOME/
+mv /home/jwno/dbqt/.xinitrc $HOME/
+mv /home/jwno/dbqt/.Xresources $HOME/
 
 # Copy .bashrc and .vimrc to root directory (same files for both user and root)
-sudo cp $HOME/.bashrc /root/
-sudo cp $HOME/.vimrc /root/
+sudo cp /home/jwno/.bashrc /root/
+sudo cp /home/jwno/.vimrc /root/
 
 # Make scripts executable
-chmod +x $HOME/.local/bin/*.sh
+chmod +x /home/jwno/.local/bin/*.sh
 
 # Configure cursor theme
 sed -i 's/Adwaita/BreezeX-RosePine-Linux/g' /usr/share/icons/default/index.theme
 
 # Cleanup
-rm -rf $HOME/dbqt/.root
+rm -rf /home/jwno/dbqt/.root
 
 echo "Themes and configuration setup complete"
